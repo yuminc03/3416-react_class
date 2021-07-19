315b3f2c1aab3c1900dbb767c443ef968a6382a5
yuminc03 <yuminc03@gamil.com>
2021-07-19 17:33:13 +0900

    2021-07-19: Hello React! 찍어보기!

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Potato from './Potato'; //Potato.js 파일을 불러옴
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React! </h1>
+      <Potato/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Potato(){
+    return <h3>♬ 나는 감자를 좋아합니다! ♬</h3>
+}
+
+export default Potato;
